947da27ecd5c6f6a643d38f62d5882b2bf68de1f
Usateid <davcir.dc@gmail.com>
2022-03-13 20:15:34 +0100

    feat: add tailwind

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,9 @@
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">A wrapper</div>
+    <div class="wrapper">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
   </header>
 
   <main>Main section</main>
