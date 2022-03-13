0dc66be19cee1199f79c2b7cd891da81205bee47
Usateid <davcir.dc@gmail.com>
2022-03-13 21:44:06 +0100

    feat: new section

---
+++ src/components/VerbSection/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div
+        class="w-full lg:w-4/6 p-8 mt-6 lg:mt-0 text-gray-900 leading-normal bg-white border border-gray-400 border-rounded"
+    >hello</div>
+</template>
+
+<script setup>
+</script>
+
+<style lang="scss" scoped>
+</style>
